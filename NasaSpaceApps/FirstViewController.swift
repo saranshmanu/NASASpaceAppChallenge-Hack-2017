6577136f29abb99465499c8753f01e97f6b4a7f1
Vansh Badkul <vanshbadkul@gmail.com>
2017-04-11 23:47:41 +0530

    Structure init

---
+++ NasaSpaceApps/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  NasaSpaceApps
+//
+//  Created by Vansh Badkul on 09/04/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+
+import UIKit
+import Alamofire
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+ 
+
+
+}
