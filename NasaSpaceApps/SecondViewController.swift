6f30c23b177ef7f91163ce9beb035522a4336a7d
Saransh Mittal <saranshmanu@yahoo.co.in>
2017-04-17 19:26:44 +0530

    deselect option added to table view in schedule

--- NasaSpaceApps/SecondViewController.swift
+++ NasaSpaceApps/SecondViewController.swift
@@ -12,6 +12,7 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
     let time = [["8:00 am", "10:00 am", "11:00 am", "1:00 pm", "2:00 pm", "4:00 pm", "8:00 pm"],["1:00 am", "6:00 am", "9:00 am", "11:00 am", "2:00 pm", "5:00 pm", "6:00 pm"]]
     let eventTitle = [["Registration Check","HACK101", "Hacking Begins", "Lunch", "HackShop Opens", "Open Mentor Hours", "Dinner"], ["Midnight Munchies", "Team Acknowledgement", "Breakfast", "Practice Presentations", "Final Presentations", "Judges Deliberation and Feedback Session", "Closing Ceremony and Results"]]
     let sections = ["Saturday, April 29th","Sunday, April 30th"]
+    let information = [["All the Registered Participants acknowledge there presence followed by simultaneous Ice-Breaking Sessions.", "NASA SpaceApps 2017 would be explained and problem statements would be discussed. All the participants would be introduced to NASA Open Data and the judging criteria.", "Participants are expected to get a sense of problem statements by now. Hack Begins. Participants would be given HackerSpace, some plug points and a good WiFi Connection.", "Take a break, Relax!", "Hardware components will be given to the Teams, if required, on a First-Come-First-Serve basis.", "Mentors from diverse backgrounds would be accessible to all the participants.", "Fuel Up!"], ["Snacks and Caffeine will be provided to the hackers to keep up with the energy!", "Final Team List would be prepared for scheduling the presentations.", "Relax For a While!", "Time will be given to better help the hackers with their own mock presentation.", "The time for the participants to Show Off their hard work done during the SpaceAppsChallenge!", "Participants will be reminded to focus on giving kind, specific and helpful feedback.", "The most awaited moment!"]]
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -35,15 +36,13 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return eventTitle[section].count
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! EventFlowTableViewCell
         cell.circleView.layer.cornerRadius = 9.5
         cell.title.text = String(eventTitle[indexPath.section][indexPath.row])
         cell.timing.text = String(time[indexPath.section][indexPath.row])
-        
-        
         return cell
     }
     
